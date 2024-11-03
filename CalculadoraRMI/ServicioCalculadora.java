b865b1b43845c01d05de18a001f3f45c9e05ff79
JesusOrtega12 <jo09131205@gmail.com>
2024-11-02 21:56:40 -0600

    Delete CalculadoraRMI directory

--- CalculadoraRMI/ServicioCalculadora.java
+++
@@ -1,9 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface ServicioCalculadora extends Remote {
-    float suma(float x, float y) throws RemoteException;
-    float resta(float x, float y) throws RemoteException;
-    float multiplicacion(float x, float y) throws RemoteException;
-    float division(float x, float y) throws RemoteException;
-}
